9d49141fc0e3177df731f544c90f5ad37bc3044a
yarainville <yrain012@uottawa.ca>
2025-12-20 21:06:15 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/grocery/grocery_price_minimizer/GroceryPriceMinimizerApplication.java
@@ -0,0 +1,13 @@
+package com.grocery.grocery_price_minimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryPriceMinimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryPriceMinimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grocery/grocery_price_minimizer/GroceryPriceMinimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocery.grocery_price_minimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryPriceMinimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
